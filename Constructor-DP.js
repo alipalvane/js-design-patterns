31b4910519bc82e80484928eee7a92f7d6f2d872
Ali <alipalvane@gmail.com>
2023-10-03 17:47:10 +0330

    initial commit

---
+++ Constructor-DP.js
@@ -0,0 +1,21 @@
+// Constructor design pattern - Creational Design
+class Person {
+  constructor(name, age, mother) {
+    this.name = name;
+    this.age = age;
+    this.mother = mother;
+  }
+}
+
+const tim = new Person('tim', 25, null)
+const tina = new Person('tina', 22, null)
+
+tim.mother = 'yuli'
+
+console.log(tim)
+
+const grandma = new Person('Sherry', 80, null);
+
+tina.mother = grandma
+
+console.log(tina)
